67ab9a34edcc8f561d349baf580db4d4e8749487
julius <phpninja@mail.com>
2020-07-12 21:46:29 -0700

    sort for people table works

--- src/ResultsList.js
+++ src/ResultsList.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react'
+import _ from 'lodash';
 import {Table} from 'semantic-ui-react'
 
 class ResultsList extends Component {
+    state = {
+        column: null,
+        data: null,
+        direction: null
+    }
+
     constructor(props) {
         super(props);
         this.state = {data: []};
@@ -13,17 +20,58 @@ class ResultsList extends Component {
             .then(data => this.setState({data: data.data}));
     }
 
+    handleSort = (clickedColumn) => () => {
+        const {column, data, direction} = this.state;
+
+        if (column !== clickedColumn) {
+            this.setState({
+                column: clickedColumn,
+                data: _.sortBy(data, [clickedColumn]),
+                direction: 'ascending',
+            })
+
+            return;
+        }
+
+        this.setState({
+            data: data.reverse(),
+            direction: direction === 'ascending' ? 'descending' : 'ascending',
+        })
+    }
+
     render() {
         let data = this.state.data || [];
+        let column = this.state.column;
+        let direction = this.state.direction;
+        let first = 'first_name', last = 'last_name', email = 'email_address', status = 'status';
 
         return (
-            <Table celled padded>
+            <Table celled padded sortable>
                 <Table.Header>
                     <Table.Row>
-                        <Table.HeaderCell singleLine>First Name</Table.HeaderCell>
-                        <Table.HeaderCell>Last Name</Table.HeaderCell>
-                        <Table.HeaderCell>Email</Table.HeaderCell>
-                        <Table.HeaderCell>Status</Table.HeaderCell>
+                        {/* First Name column */}
+                        <Table.HeaderCell
+                            sorted={column === first ? direction : null}
+                            onClick={this.handleSort(first)}>
+                            First Name</Table.HeaderCell>
+
+                        {/* Last Name column */}
+                        <Table.HeaderCell
+                            sorted={column === last ? direction : null}
+                            onClick={this.handleSort(last)}>
+                            Last Name</Table.HeaderCell>
+
+                        {/* Email column */}
+                        <Table.HeaderCell
+                            sorted={column === email ? direction : null}
+                            onClick={this.handleSort(email)}>
+                            Email</Table.HeaderCell>
+
+                        {/* Status column */}
+                        <Table.HeaderCell
+                            sorted={column === status ? direction : null}
+                            onClick={this.handleSort(status)}>
+                            Status</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
 
